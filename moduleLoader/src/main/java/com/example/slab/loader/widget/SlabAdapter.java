b047625e9010dd7a3dc5ef95356fa321e09af106
dagbagdog <hgleifeng@foxmail.com>
2016-08-21 18:10:26 +0800

    decoration 做recyclerView的点击事件拦截

--- moduleLoader/src/main/java/com/example/slab/loader/widget/SlabAdapter.java
+++ moduleLoader/src/main/java/com/example/slab/loader/widget/SlabAdapter.java
@@ -2,9 +2,11 @@ package com.example.slab.loader.widget;
 
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -14,9 +16,13 @@ public class SlabAdapter<T> extends RecyclerView.Adapter {
      * @param <VH> the type of the view holder
      * @param <T> the type on Adapter's item model
      */
-    public interface ArrayAdapterDelegate<VH extends ViewHolder, T> {
+    public interface ArrayAdapterDelegate<VH, T> {
         VH onCreateViewHolder(SlabAdapter<T> adapter, ViewGroup parent, int viewType);
         void onBindViewHolder(SlabAdapter<T> adapter, ViewHolder holder, int position);
+        int getItemViewType(SlabAdapter<T> adapter, int position);
+        /** 当设置hasStableIds为true时被调用， 通常id和positon无关时**/
+        long getItemId(SlabAdapter<T> adapter, int position);
+
     }
     private static final String TAG = "SlabAdapter";
     List<T> mList;
@@ -38,6 +44,10 @@ public class SlabAdapter<T> extends RecyclerView.Adapter {
         delegate.onBindViewHolder(this, holder, position);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return delegate.getItemViewType(this, position);
+    }
 
     @Override
     public int getItemCount() {
@@ -53,6 +63,11 @@ public class SlabAdapter<T> extends RecyclerView.Adapter {
     public final void clearDataSet() {
         notifyItemRangeRemoved(0, mList.size());
         mList.clear();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return delegate.getItemId(this, position);
     }
 
     public final T getItem(int position) {
@@ -73,6 +88,14 @@ public class SlabAdapter<T> extends RecyclerView.Adapter {
         mList.remove(position);
         notifyItemRemoved(position);
     }
+
+    public final void moveItem(int fromPostion, int toPositon) {
+        Log.d(TAG, "moveItem() called with: " + "fromPostion = [" + fromPostion + "], toPositon = [" + toPositon + "]");
+        Collections.swap(mList, fromPostion, toPositon);
+        notifyItemMoved(fromPostion, toPositon);
+    }
+
+
 
 
 }
